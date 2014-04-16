c330a5ac20bc217da912e51a8fda724dcf2d7fb7
Thierry JACQUELINE <jacquelineth@gmail.com>
2014-04-16 23:33:53 +0200

    Restore/Clean environment

--- bin/clean.bat
+++ bin/clean.bat
@@ -1,12 +1,20 @@
-
+@echo off
+if "%OS%" == "Windows_NT" setlocal
 
 set "CURRENT_DIR=%cd%"
-echo %CURRENT_DIR%
+rem echo %CURRENT_DIR%
 set CATALINA_BASE=%~dp0%..
-echo CATALINA_BASE=%CATALINA_BASE%
+echo processing wtih CATALINA_BASE=%CATALINA_BASE%
 pushd %CATALINA_BASE%
 rmdir /S /Q logs
 rmdir /S /Q  temp
 rmdir /S /Q  work
 mkdir logs temp work
+echo Cleaned logs temp work
+
+if ""%1"" == """" goto noData
+rmdir /S /Q data
+mkdir data
+echo Cleaned data
+:noData
 popd
