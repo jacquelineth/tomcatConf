149d0286ad8c078f2550122e17efd02c841b651d
Thierry JACQUELINE <jacquelineth@gmail.com>
2015-05-08 15:07:13 +0200

    Corrected server.xml to distribute Context in Hosts as recommended by the doc

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -1,3 +1,4 @@
 set CATALINA_HOME=C:\DevTools\apache-tomcat-7.0.50
 set JAVA_HOME=C:\jdk1.7.0_60
-
+set ODM87_HOME=C:\IBM\ODM87
+set CATLINA_OPTS= -d64 -DODM87.home=%ODM87_HOME% -DCatalina.base=%CATALINA_BASE%
