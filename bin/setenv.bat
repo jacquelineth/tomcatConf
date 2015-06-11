4df071328f95e07e2c9b830f720b4ef26b613f79
Thierry JACQUELINE <jacquelineth@gmail.com>
2015-06-11 12:18:39 +0200

    Upgrade path from  [8.7] to [8.7.1]

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -1,4 +1,4 @@
 set CATALINA_HOME=C:\DevTools\apache-tomcat-7.0.50
 set JAVA_HOME=C:\jdk1.7.0_60
-set ODM87_HOME=C:\IBM\ODM87
+set ODM87_HOME=C:\IBM\ODM871
 set CATLINA_OPTS= -d64 -DODM87.home=%ODM87_HOME% -DCatalina.base=%CATALINA_BASE%
