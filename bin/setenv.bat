ea9a2d907ef2d63f0189205ea131f255d8e647f8
Thierry JACQUELINE <jacquelineth@gmail.com>
2015-11-29 03:26:46 +0100

    Cleaned and Commented  JMX config
    Conflicts:
    	bin/setenv.bat
    	bin/startup.bat
    	conf/server.xml

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -1,2 +1,4 @@
-set CATALINA_HOME=C:\DevTools\apache-tomcat-7.0.41
-set JAVA_HOME=C:\jdk1.7.0_25
+set CATALINA_HOME=C:\DevTools\apache-tomcat-7.0.67
+set JAVA_HOME=C:\jdk1.8.0_66
+
+set JAVA_OPTS= -server -Xms256m -Xmx1024m -XX:MaxPermSize=512m 	-Dcom.sun.management.jmxremote.password.file=%CATALINA_BASE%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_BASE%\conf\jmxremote.access -Dcom.sun.management.jmxremote.ssl=false
