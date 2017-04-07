3b37f848b6314498102167b4810acd05be6ce945
Thierry JACQUELINE <jacquelineth@gmail.com>
2017-04-07 23:23:44 +0200

    Stable version for all WARs with different  datasources

--- bin/startDerby.bat
+++ bin/startDerby.bat
@@ -1,4 +1,4 @@
 CALL %~dp0%setenv.bat
-set DERBY_HOME=C:\DevTools\apache-derby-10.10
-set PATH=%DERBY_HOME%\bin;%PATH%
+set DERBY_HOME=C:\DevTools\apache-derby-10.12
+set PATH=%DERBY_HOME%\bin;%JAVA_HOME%\bin;C:\Windows\System;C:\Windows\System32
 java -jar %DERBY_HOME%\lib\derbyrun.jar server start
