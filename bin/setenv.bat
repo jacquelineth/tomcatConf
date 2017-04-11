859af9da38a2b5f496d3dd980aed5b2004da280f
Thierry JACQUELINE <jacquelineth@gmail.com>
2017-04-11 15:50:59 +0200

    ODM 89 Changes:
    - switch to tomcat 8
    - added Derby locales
    - removed unused datasource
    - remove JSP Jasper

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -1,15 +1,17 @@
 REM Adjust these variables
-set CATALINA_HOME=C:\DevTools\apache-tomcat-7.0.67
+set CATALINA_HOME=C:\DevTools\apache-tomcat-8.0.41
 set JAVA_HOME=C:\jdk1.8.0_121
-set ODM_HOME=C:\IBM\ODM88
+set ODM_HOME=C:\IBM\ODM89
 set PATH=.;%JAVA_HOME%\bin;%CATALINA_HOME%\bin;C:\Windows\System32;C:\Windows\System
+
 
 REM Computed
 set CATALINA_BASE=%~dp0%..
+set platform=tomcat8
 set executionserver.home=%ODM_HOME%\executionserver
-set executionserver.home.tomcat=%executionserver.home%\applicationservers\tomcat7
+set executionserver.home.tomcat=%executionserver.home%\applicationservers\%platform%
 set teamserver.home=%ODM_HOME%\teamserver
-set teamserver.home.tomcat=%teamserver.home%\applicationservers\tomcat7
+set teamserver.home.tomcat=%teamserver.home%\applicationservers\%platform%
 
 REM Hidden feature ref http://wiki.francelab.fr.ibm.com/bin/view/Lab/Rules/HiddenDebuggingFeatures
 set RTS_OPTS= -Dilog.rules.teamserver.sync.debug=true 
