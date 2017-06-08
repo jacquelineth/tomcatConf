310987de1ac222369ef26ab813a132822c33f478
Thierry JACQUELINE <jacquelineth@gmail.com>
2017-06-08 11:24:29 +0200

    Fine tune cluster : add a new server.

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -18,6 +18,6 @@ REM Hidden feature ref http://wiki.francelab.fr.ibm.com/bin/view/Lab/Rules/Hidde
 set RTS_OPTS= -Dilog.rules.teamserver.sync.debug=true 
 
 rem 64-Bit Server VM warning: ignoring option MaxPermSize=512m; support was removed in 8.0
-set JAVA_OPTS= -server -Xms1024m -Xmx4096m   -Dteamserver.home.tomcat=%teamserver.home.tomcat% -Dexecutionserver.home.tomcat=%executionserver.home.tomcat%	-Dcom.sun.management.jmxremote.password.file=%CATALINA_BASE%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_BASE%\conf\jmxremote.access -Dcom.sun.management.jmxremote.ssl=false %RTS_OPTS%
+set JAVA_OPTS= -server -Xms1024m -Xmx4096m -Dfile.encoding=UTF-8 -Dteamserver.home.tomcat=%teamserver.home.tomcat% -Dexecutionserver.home.tomcat=%executionserver.home.tomcat%	-Dcom.sun.management.jmxremote.password.file=%CATALINA_BASE%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_BASE%\conf\jmxremote.access -Dcom.sun.management.jmxremote.ssl=false %RTS_OPTS%
 set CATLINA_OPTS= -d64
 
