1e79bc671cfe4c3e1efc15988e9170148d9f8d85
Thierry JACQUELINE <jacquelineth@gmail.com>
2017-06-21 01:05:48 +0200

    Update to latest Jdk8. Added more to .gitignore.

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -1,5 +1,5 @@
 set CATALINA_HOME=C:\DevTools\apache-tomcat-7.0.67
-set JAVA_HOME=C:\jdk1.8.0_66
+set JAVA_HOME=C:\jdk1.8.0_121
 rem 64-Bit Server VM warning: ignoring option MaxPermSize=512m; support was removed in 8.0
 set JAVA_OPTS= -server -Xms256m -Xmx1024m  	-Dcom.sun.management.jmxremote.password.file=%CATALINA_BASE%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_BASE%\conf\jmxremote.access -Dcom.sun.management.jmxremote.ssl=false
 set CATLINA_OPTS= -d64 
