265b12337de0c6f803bd3ca1ed7dfb82ef4f4df4
Thierry JACQUELINE <jacquelineth@gmail.com>
2017-10-09 16:44:13 +0200

    Minor updates, thrid parties version upgrade

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -1,5 +1,5 @@
 REM Adjust these variables
-set CATALINA_HOME=C:\DevTools\apache-tomcat-7.0.67
+set CATALINA_HOME=C:\DevTools\apache-tomcat-7.0.81
 set JAVA_HOME=C:\jdk1.8.0_121
 set ODM_HOME=C:\IBM\ODM88
 set PATH=.;%JAVA_HOME%\bin;%CATALINA_HOME%\bin;C:\Windows\System32;C:\Windows\System
@@ -15,5 +15,5 @@ REM Hidden feature ref http://wiki.francelab.fr.ibm.com/bin/view/Lab/Rules/Hidde
 set RTS_OPTS= -Dilog.rules.teamserver.sync.debug=true 
 
 rem 64-Bit Server VM warning: ignoring option MaxPermSize=512m; support was removed in 8.0
-set JAVA_OPTS= -server -Xms1024m -Xmx4096m   -Dteamserver.home.tomcat=%teamserver.home.tomcat% -Dexecutionserver.home.tomcat=%executionserver.home.tomcat%	-Dcom.sun.management.jmxremote.password.file=%CATALINA_BASE%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_BASE%\conf\jmxremote.access -Dcom.sun.management.jmxremote.ssl=false %RTS_OPTS%
+set JAVA_OPTS= -server -Xms1024M  -Xmx8192M    -Dteamserver.home.tomcat=%teamserver.home.tomcat% -Dexecutionserver.home.tomcat=%executionserver.home.tomcat%	-Dcom.sun.management.jmxremote.password.file=%CATALINA_BASE%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_BASE%\conf\jmxremote.access -Dcom.sun.management.jmxremote.ssl=false %RTS_OPTS%
 set CATLINA_OPTS= -d64
--- bin/startDerby.bat
+++ bin/startDerby.bat
@@ -1,4 +1,4 @@
 CALL %~dp0%setenv.bat
-set DERBY_HOME=C:\DevTools\apache-derby-10.12
+set DERBY_HOME=C:\DevTools\apache-derby-10.13
 set PATH=%DERBY_HOME%\bin;%JAVA_HOME%\bin;C:\Windows\System;C:\Windows\System32
 java -jar %DERBY_HOME%\lib\derbyrun.jar server start
