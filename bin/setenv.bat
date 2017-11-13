29b26682e166220477ed0c6d88e03e1075b4fe34
Thierry JACQUELINE <jacquelineth@gmail.com>
2017-11-13 14:37:10 +0100

    Displaying classic rule projects in the Business console https://www.ibm.com/support/knowledgecenter/SSQP76_8.9.0/com.ibm.odm.dcenter.admin/topics/tsk_dcbc_view_crp.html

--- bin/setenv.bat
+++ bin/setenv.bat
@@ -15,5 +15,5 @@ REM Hidden feature ref http://wiki.francelab.fr.ibm.com/bin/view/Lab/Rules/Hidde
 set RTS_OPTS= -Dilog.rules.teamserver.sync.debug=true 
 
 rem 64-Bit Server VM warning: ignoring option MaxPermSize=512m; support was removed in 8.0
-set JAVA_OPTS= -server -Xms1024m -Xmx4096m   -Dteamserver.home.tomcat=%teamserver.home.tomcat% -Dexecutionserver.home.tomcat=%executionserver.home.tomcat%	-Dcom.sun.management.jmxremote.password.file=%CATALINA_BASE%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_BASE%\conf\jmxremote.access -Dcom.sun.management.jmxremote.ssl=false %RTS_OPTS%
+set JAVA_OPTS= -server -Xms1024m -Xmx4096m   -Dteamserver.home.tomcat=%teamserver.home.tomcat% -Dexecutionserver.home.tomcat=%executionserver.home.tomcat%	-Dcom.sun.management.jmxremote.password.file=%CATALINA_BASE%\conf\jmxremote.password -Dcom.sun.management.jmxremote.access.file=%CATALINA_BASE%\conf\jmxremote.access -Dcom.sun.management.jmxremote.ssl=false %RTS_OPTS% -Dcom.ibm.rules.decisioncenter.enableClassicRuleProjectSupport=true
 set CATLINA_OPTS= -d64
